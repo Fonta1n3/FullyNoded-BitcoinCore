110511e62078a85fa2403a0daac78910a159aacc
Fonta1n3 <fontainedenton@googlemail.com>
2023-07-08 12:15:54 -0500

    refactor: add currencies class

---
+++ FullyNoded/Helpers/Currencies.swift
@@ -0,0 +1,37 @@
+//
+//  Currencies.swift
+//  FullyNoded
+//
+//  Created by Peter Denton on 7/8/23.
+//  Copyright © 2023 Fontaine. All rights reserved.
+//
+
+import Foundation
+
+class Currencies {
+    static let currencies:[[String:String]] = [
+        ["USD": "dollarsign.circle"],
+        ["GBP": "sterlingsign.circle"],
+        ["EUR": "eurosign.circle"],
+        ["AUD":"dollarsign.circle"],
+        ["BRL": "brazilianrealsign.circle"],
+        ["CAD": "dollarsign.circle"],
+        ["CHF": "francsign.circle"],
+        ["CLP": "dollarsign.circle"],
+        ["CNY": "yensign.circle"],
+        ["DKK": "k.circle"],
+        ["HKD": "dollarsign.circle"],
+        ["INR": "indianrupeesign.circle"],
+        ["ISK": "k.circle"],
+        ["JPY": "yensign.circle"],
+        ["KRW": "wonsign.circle"],
+        ["NZD": "dollarsign.circle"],
+        ["PLN": "z.circle"],
+        ["RUB": "rublesign.circle"],
+        ["SEK": "k.circle"],
+        ["SGD": "dollarsign.circle"],
+        ["THB": "bahtsign.circle"],
+        ["TRY": "turkishlirasign.circle"],
+        ["TWD": "dollarsign.circle"]
+    ]
+}
